8d002f306c5549d6ff07d219f229d956b0c0bbae
VarunSaiBaddapuram <weirdovarun@gmail.com>
2024-02-05 22:01:29 +0530

    Messaging App

---
+++ client/src/Logo.jsx
@@ -0,0 +1,11 @@
+export default function Logo() {
+  return (
+    <div className="text-blue-600 font-bold flex gap-2 p-4">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+      
+        
+      </svg>
+      Hello!!
+    </div>
+  );
+}
